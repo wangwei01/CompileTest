7eb9635e8739b0a99ff31b56b9bf6c62cd04f4c2
wangwei <wangwei@timeface.cn>
2018-01-23 10:12:45 +0800

    添加准备生成的依赖包。。。。

---
+++ mycompilelibrary/src/main/java/com/example/mycompilelibrary/CompileTestThing.java
@@ -0,0 +1,13 @@
+package com.example.mycompilelibrary;
+
+/**
+ * Created by wangwei on 2018/1/23.
+ */
+
+public class CompileTestThing {
+
+    public String getBeautifulThing() {
+        return "Hello World";
+    }
+
+}
